5d82a50f1a18000b3a52774fbecb9a979fee645e
devd7c <*elmejor123*>
2018-10-12 13:31:32 -0400

    fixeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -41,7 +41,7 @@ class DatabaseSeeder extends Seeder
 
     public function run()
     {
-        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        // DB::statement('SET FOREIGN_KEY_CHECKS = 0');
 
         /*User::truncate();
         Category::truncate();
